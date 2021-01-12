eb1bee95056bbf65e2d2ec9abcf54e78f3ebc6f2
Ecophagy <olly.spoon@gmail.com>
2021-01-12 18:19:52 +0000

    Post parsing from HTML
    Beautiful Soup is pretty great!

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import requests
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from bs4 import BeautifulSoup
+from post import Post
 
 
-# Press the green button in the gutter to run the script.
+def download_posts(game_url, player_name):
+    r = requests.get(game_url)
+    total_soup = BeautifulSoup(r.content, 'html.parser')
+
+    posts_soup = total_soup.find(id='comments')
+
+    posts = []
+    for post_soup in posts_soup.find_all('li', class_='p-comments p-comments-b'):
+        poster_name = post_soup.find('span', itemprop='name')
+        post_content = post_soup.find('div', class_='j-comment-body-container p-comment-body forum-post-body-content')
+        post = Post(poster=poster_name.string, content=post_content.text)
+        posts.append(post)
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    download_posts('https://www.mtgsalvation.com/forums/community-forums/mafia/819846-shoushiling-mafia-day-2', 'TODO')
---
+++ post.py
@@ -0,0 +1,5 @@
+
+class Post:
+    def __init__(self, poster, content):
+        self.poster = poster
+        self.content = content
